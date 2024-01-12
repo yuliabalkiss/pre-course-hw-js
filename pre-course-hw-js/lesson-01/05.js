062078ba76fd73b31188427e5806be3f579652ac
yuliabalkiss <balkisula451@gmail.com>
2024-01-12 12:49:31 +0300

    add file 05.js

--- pre-course-hw-js/lesson-01/05.js
+++ pre-course-hw-js/lesson-01/05.js
@@ -1,7 +1,18 @@
-let x = 5;
-x += 3;
-x *= 2;
-x -= 4;
-x /= 3;
-let lastArg = x
-console.log(lastArg)
+let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+
+switch (rule.length) {
+    case rule.length < 25:
+        console.log("Все таки нет правил без исключения");
+        break;
+
+    case (rule.length > 25):
+        console.log("У меня 100 пудов все получится");
+break;
+    case  25:
+        console.log('50 на 50')
+
+        break;
+
+    default:
+            console.log(rule)
+}

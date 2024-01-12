d3549a360efad00d980af546b47a77174df3b08e
yuliabalkiss <balkisula451@gmail.com>
2024-01-12 14:32:55 +0300

    find bug

--- pre-course-hw-js/lesson-01/06.js
+++ pre-course-hw-js/lesson-01/06.js
@@ -1,5 +1,6 @@
 let str = "zzzzyzz";
 function findSymbol (str){
-   return  str.includes('y') ? console.log('нашел') : console.log('заблудился')
+   console.log(str.includes('y') ? 'нашел' : 'заблудился')
 
 }
+findSymbol(str);

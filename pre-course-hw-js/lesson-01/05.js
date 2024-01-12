0dbfce80dbc3010c31e017fa6c8d0c9262817eb2
yuliabalkiss <balkisula451@gmail.com>
2024-01-12 13:09:01 +0300

    Fixed bug in rule

--- pre-course-hw-js/lesson-01/05.js
+++ pre-course-hw-js/lesson-01/05.js
@@ -8,7 +8,7 @@ switch (true) {
     case (rule.length > 25):
         console.log("У меня 100 пудов все получится");
 break;
-    case  25:
+    case (rule.length ===25) :
         console.log('50 на 50')
 
         break;

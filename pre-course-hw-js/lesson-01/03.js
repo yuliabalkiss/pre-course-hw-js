e397005d388217a68322fb37d7fcb859f180256a
yuliabalkiss <balkisula451@gmail.com>
2024-01-12 12:05:25 +0300

    change file 03.js

--- pre-course-hw-js/lesson-01/03.js
+++ pre-course-hw-js/lesson-01/03.js
@@ -1,9 +1,6 @@
 let number = 1;
-console.log(number)
-number++;
-++number;
-console.log(number)
-console.log(number)
---number
---number
-console.log(number)
+
+console.log(number++)
+console.log(++number)
+console.log(number--)
+console.log(--number)

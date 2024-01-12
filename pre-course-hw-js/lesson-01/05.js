21c75e9608b249514e8e36185838b47acb212710
yuliabalkiss <balkisula451@gmail.com>
2024-01-12 13:52:39 +0300

    Fixed bug in rule

--- pre-course-hw-js/lesson-01/05.js
+++ pre-course-hw-js/lesson-01/05.js
@@ -1,21 +1,22 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
- function  checkRuleLength(rule){
+
+function  checkRuleLength(rule){
      switch (true) {
          case (rule.length < 25):
-             return "Все таки нет правил без исключения";
-
+             console.log("Все таки нет правил без исключения");
+break;
 
          case (rule.length > 25):
-             return "У меня 100 пудов все получится";
-
+             console.log("У меня 100 пудов все получится");
+             break;
          case (rule.length === 25):
-             return '50 на 50';
+             console.log('50 на 50');
 
-
+             break;
 
          default:
-             return rule;
+             console.log("А что так можно было?");
      }
  }
 
-console.log(checkRuleLength(rule));
+checkRuleLength(rule);

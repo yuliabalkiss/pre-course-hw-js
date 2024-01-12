2c00935b5fc39a58a5a4293fd34a83841ba2f22e
yuliabalkiss <balkisula451@gmail.com>
2024-01-12 12:56:29 +0300

    change variable

--- pre-course-hw-js/lesson-01/05.js
+++ pre-course-hw-js/lesson-01/05.js
@@ -1,7 +1,7 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
 switch (rule.length) {
-    case rule.length < 25:
+    case (rule.length < 25):
         console.log("Все таки нет правил без исключения");
         break;
 

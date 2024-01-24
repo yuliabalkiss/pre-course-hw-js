adbfede9efc5d2e1ee7d461090f0df21b9d693e7
yuliabalkiss <balkisula451@gmail.com>
2024-01-24 12:20:28 +0300

    changes files

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,40 @@
-
-// data
-const info = {
-    title: 'it-incubator',
-    graduatesCount: 2000,
-    areYouChampion: true,
-    technologies: [
-        {title: 'Front'},
-        {title: 'Back'},
-        {title: 'DevOps'},
-
-      ],
-};
-
-
-
-// render
-    let pageTitleElement = document.createElement('h1');
-    pageTitleElement.innerText = info.title;
-    document.body.append(pageTitleElement);
-
-    let graduatesCountElement = document.createElement('input');
-    graduatesCountElement.value = info.graduatesCount;
-    document.body.append(graduatesCountElement);
-
-    let areYouChampionElement = document.createElement('input');
-    areYouChampionElement.type = 'checkbox';
-    areYouChampionElement.checked = info.areYouChampion;
-
-    document.body.append(areYouChampionElement);
-    let  techSelectElement = document.createElement('select');
-    let techSelectOption0Element = document.createElement('option')
-    techSelectOption0Element.append(info.technologies[0].title)
-   let techSelectOption1Element = document.createElement('option')
-    techSelectOption1Element.append(info.technologies[1].title)
-     let techSelectOption2Element = document.createElement('option')
-    techSelectOption2Element.append(info.technologies[2].title)
+//
+// // data
+// const info = {
+//     title: 'it-incubator',
+//     graduatesCount: 2000,
+//     areYouChampion: true,
+//     technologies: [
+//         {title: 'Front'},
+//         {title: 'Back'},
+//         {title: 'DevOps'},
+//
+//       ],
+// };
+//
+//
+//
+// // render
+//     let pageTitleElement = document.createElement('h1');
+//     pageTitleElement.innerText = info.title;
+//     document.body.append(pageTitleElement);
+//
+//     let graduatesCountElement = document.createElement('input');
+//     graduatesCountElement.value = info.graduatesCount;
+//     document.body.append(graduatesCountElement);
+//
+//     let areYouChampionElement = document.createElement('input');
+//     areYouChampionElement.type = 'checkbox';
+//     areYouChampionElement.checked = info.areYouChampion;
+//
+//     document.body.append(areYouChampionElement);
+//     let  techSelectElement = document.createElement('select');
+//     let techSelectOption0Element = document.createElement('option')
+//     techSelectOption0Element.append(info.technologies[0].title)
+//    let techSelectOption1Element = document.createElement('option')
+//     techSelectOption1Element.append(info.technologies[1].title)
+//      let techSelectOption2Element = document.createElement('option')
+//     techSelectOption2Element.append(info.technologies[2].title)
 
     techSelectElement.append(techSelectOption0Element,techSelectOption1Element,techSelectOption2Element)
     document.body.append(techSelectElement)
--- JS/player.js
+++ JS/player.js
@@ -1,6 +1,6 @@
 //data
 
-let player = {
+let playList = {
     title: 'Hip-Hop Hits',
     coverImageUrl: './hip-hop-hits.jpg',
     info: {
@@ -10,14 +10,16 @@ let player = {
     tracks:[
 
         {
-            coverImageUrl: ''
-            artist:'Eminem',
+            coverImageUrl: 'track1.png',
+            artistName:'Eminem',
             title: 'Rap God',
             fileUrl:'Eminem_-_Rap_God.mp3',
             isHot: false,
         },
         {
-            artist:'50cent',
+
+            coverImageUrl: 'track2.mp3',
+            artistName:'50cent',
             title: 'In da Club',
             fileUrl:'50_Cent_-_In_Da_Club_FlexyOkay.mp3',
             isHot: true,
@@ -25,3 +27,29 @@ let player = {
     ]
 }
 // render
+
+renderPlayList(playList)
+function renderPlayList(playListForRendering){
+    let playListImgElement = document.createElement('img');
+    playListImgElement.src = playList.coverImageUrl;
+    document.body.append(playListImgElement);
+
+    let playListTitleElement = document.createElement('h2');
+    playListTitleElement.append(playList.title);
+    document.body.append(playListTitleElement);
+
+    let tracksListElement = document.createElement('ul');
+
+    let track1ListElement = document.createElement('li')
+    track1ListElement.append(playList.tracks[0].artistName + ' - ' + playList.tracks[0].title)
+
+    let track2ListElement = document.createElement('li');
+    track2ListElement.append(playList.tracks[1].artistName + ' - ' + playList.tracks[1].title)
+
+    tracksListElement.append(track1ListElement)
+    tracksListElement.append(track2ListElement)
+
+    document.body.append(tracksListElement)
+
+}
+

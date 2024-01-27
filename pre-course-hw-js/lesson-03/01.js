941e5429ba5833f0fb3ef3b29216d559d51d0303
yuliabalkiss <balkisula451@gmail.com>
2024-01-28 00:49:52 +0300

    change file 01.js

--- pre-course-hw-js/lesson-03/01.js
+++ pre-course-hw-js/lesson-03/01.js
@@ -1,14 +1,23 @@
 // code
+// const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+//
+// function createColorString() {
+//     let GermanyFlagColors = []
+//     for(let i = 0; i<colors.length; i++){
+//         if(colors[i] === 'черный' || colors[i]=== 'красный' || colors[i]=== 'желтый'){
+//             GermanyFlagColors.push(colors[i])
+//         }
+//     }
+//     return GermanyFlagColors.join('-')
+// }
+//
+// console.log(createColorString(colors));
+
+// Второй вариант решения
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    let GermanyFlagColors = []
-    for(let i = 0; i<colors.length; i++){
-        if(colors[i] === 'черный' || colors[i]=== 'красный' || colors[i]=== 'желтый'){
-            GermanyFlagColors.push(colors[i])
-        }
-    }
-    return GermanyFlagColors.join('-')
+    return colors.filter(item => item === 'черный' || item === 'красный' || item === 'желтый').join('-')
 }
 
 console.log(createColorString(colors));

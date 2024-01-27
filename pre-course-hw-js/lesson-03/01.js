c3543966aa15d048fc394b2b1e14650e9700e2c5
yuliabalkiss <balkisula451@gmail.com>
2024-01-28 00:42:16 +0300

    add file 01.js

--- pre-course-hw-js/lesson-03/01.js
+++ pre-course-hw-js/lesson-03/01.js
@@ -1,1 +1,14 @@
 // code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    let GermanyFlagColors = []
+    for(let i = 0; i<colors.length; i++){
+        if(colors[i] === 'черный' || colors[i]=== 'красный' || colors[i]=== 'желтый'){
+            GermanyFlagColors.push(colors[i])
+        }
+    }
+    return GermanyFlagColors.join('-')
+}
+
+console.log(createColorString(colors));

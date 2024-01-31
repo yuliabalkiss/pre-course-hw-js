4d50475d97237ab423d399525c5c918d784eae34
yuliabalkiss <balkisula451@gmail.com>
2024-01-31 23:51:06 +0300

    fixed bug in  file 02.js

--- pre-course-hw-js/lesson-03/02.js
+++ pre-course-hw-js/lesson-03/02.js
@@ -1,11 +1,8 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 
 function findOrange(arr){
-    if (arr.indexOf("апельсин") !== -1) {
-        console.log("Ура! нашел");
-    } else {
-        console.log("Придется поискать в другом магазине…");
-    }
+    return arr.indexOf("апельсин") !== -1 ? "Ура! нашел" : "Придется поискать в другом магазине…"
+
 }
 
 findOrange(words);

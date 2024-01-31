74679386dfe6800cdc69802f900394be75df75da
yuliabalkiss <balkisula451@gmail.com>
2024-01-31 23:21:21 +0300

    fixed bug in  file 02.js

--- pre-course-hw-js/lesson-03/02.js
+++ pre-course-hw-js/lesson-03/02.js
@@ -1,7 +1,15 @@
+// const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+//
+// function findOrange(){
+//     console.log(words.indexOf("апельсин") ? "Ура! нашел" : "Придется поискать в другом магазине...")
+// }
+//
+// findOrange(words)
+
+
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-
-function findOrange(){
-    console.log(words.indexOf("апельсин") ? "Ура! нашел" : "Придется поискать в другом магазине...")
+if (words.indexOf("апельсин") !== -1) {
+    console.log("Ура! нашел");
+} else {
+    console.log("Придется поискать в другом магазине…");
 }
-
-findOrange(words)

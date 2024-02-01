068c79c942d1008c7b7b299374f44e46cd840f36
yuliabalkiss <balkisula451@gmail.com>
2024-02-01 16:21:51 +0300

    fixed bug in  file 02.js

--- pre-course-hw-js/lesson-03/02.js
+++ pre-course-hw-js/lesson-03/02.js
@@ -1,9 +1,8 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-
 const index = words.indexOf("апельсин");
-    if(index !== -1){
-        console.log("Ура!нашел");
-    }else{
+    if (index !== -1){
+        console.log("Ура! нашел");
+    } else {
         console.log("Придется поискать в другом магазине…");
     }
 

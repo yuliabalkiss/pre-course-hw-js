c3b7999f0e28c19d0f8baa3a26c43d41adf4f398
yuliabalkiss <balkisula451@gmail.com>
2024-02-01 20:58:44 +0300

    fixed bug in  file 02.js

--- pre-course-hw-js/lesson-03/02.js
+++ pre-course-hw-js/lesson-03/02.js
@@ -1,16 +1,11 @@
-// const words =["яблоко", "банан", "апельсин", "манго", "киви"];
-// function foo(arr) {
-//     if (arr.indexOf("апельсин") !== -1) {
-//       console.log("Ура! нашел");
-//     } else {
-//         console.log("Придется поискать в другом магазине…");
-//     }
-//     return true
-// }
-// foo(words);
-
 const words =["яблоко", "банан", "апельсин", "манго", "киви"];
 function foo(arr) {
-return arr.indexOf("апельсин")!== -1 ? "Ура! нашел" :"Придется поискать в другом магазине…"
+    if (arr.indexOf("апельсин") !== -1) {
+      console.log("Ура! нашел");
+    } else {
+        console.log("Придется поискать в другом магазине…");
+    }
+    return true
 }
 foo(words);
+

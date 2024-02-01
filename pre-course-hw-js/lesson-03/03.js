f0cdc89b8371c404bc78cb145f4cd8c3af5823d1
yuliabalkiss <balkisula451@gmail.com>
2024-02-01 21:18:08 +0300

    change file 03.js

--- pre-course-hw-js/lesson-03/03.js
+++ pre-course-hw-js/lesson-03/03.js
@@ -1,6 +1,5 @@
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
 function foo (arr){
     return  arr.slice(1,-2)
-
 }
 console.log(foo(fruits));

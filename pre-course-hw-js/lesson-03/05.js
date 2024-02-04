5b0ffb1b1131c7642dd40b7f1b9f342be3a506c4
yuliabalkiss <balkisula451@gmail.com>
2024-02-04 17:42:29 +0300

    add file 05.js

---
+++ pre-course-hw-js/lesson-03/05.js
@@ -0,0 +1,33 @@
+const users2 = [
+    {
+        id: 1,
+        user: 'John',
+        surName: 'Doe',
+        address: '123 Main St'
+    },
+    {
+        id: 2,
+        user: 'Jane',
+        surName: 'Smith',
+        address: '456 Elm St'
+    },
+    {
+        id: 3,
+        user: 'Tom',
+        surName: 'Johnson',
+        address: '789 Oak St'
+    },
+];
+
+const newObj = {
+    id: 4,
+    user: 'Ivan',
+    surName: 'Susanin',
+    address: 'Unknown'
+}
+
+
+function foo (arr, arr2){
+   return  arr.arr2 = arr2
+}
+console.log(foo(users2, newObj))

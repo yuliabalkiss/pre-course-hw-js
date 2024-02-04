1563c483fade57eabaea196444101f8f96dc0b84
yuliabalkiss <balkisula451@gmail.com>
2024-02-05 00:19:46 +0300

    add files styles.css index.js index.html

---
+++ clock/index.js
@@ -0,0 +1,28 @@
+let timerId; // переменная, которая будет хранить ID таймера
+
+
+
+const startButton = document.getElementById("start");
+
+startButton.addEventListener('click', function () {
+    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
+    timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+});
+
+
+const stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', function () {
+    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
+    clearInterval(timerId); // останавливаем таймер
+});
+
+
+function updateClock() {
+    const clock = document.getElementById('clock');
+    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
+    const now = new Date();
+    const hours = now.getHours()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
+    clock.textContent = hours + ':' + minutes + ':' + seconds;
+}
